984cf8ad523cd0e9b0e222877a89ab617fc3ee93
Frankie <frankie@gmail.com>
2023-12-30 10:17:14 +0700

    Defer stacking

--- defer.go
+++ defer.go
@@ -5,4 +5,13 @@ import "fmt"
 func def() {
 	defer fmt.Println("World")
 	fmt.Println("Hello")
+
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+
 }
