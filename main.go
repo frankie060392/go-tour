07278377c8e837500767708d0653587feb1d5415
Frankie <frankie@gmail.com>
2023-12-30 09:44:42 +0700

    Type interence

--- main.go
+++ main.go
@@ -12,4 +12,5 @@ func main() {
 	shortVar()
 	types()
 	typeConversion()
+	typeInterence()
 }
---
+++ type_inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func typeInterence() {
+	v := 42
+	fmt.Printf("V is of type %T\n", v)
+}
