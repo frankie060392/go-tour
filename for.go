acc576420fceb7080df63a398bbfcd78a9c7d926
Frankie <frankie@gmail.com>
2023-12-30 10:00:03 +0700

    for loop options

--- for.go
+++ for.go
@@ -1,9 +1,18 @@
 package main
 
+import "fmt"
+
 func forLoop() {
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += i
 	}
 	println(sum)
+	sum1 := 1
+	for sum1 < 10 {
+		println(sum1)
+		sum1 += sum1
+	}
+	fmt.Println(sum1)
+
 }
