36bc94aa498f32c1892b63f8a32de7aa27a87497
Frankie <frankie@gmail.com>
2023-12-30 17:31:14 +0700

    Map literals

--- maps.go
+++ maps.go
@@ -8,10 +8,18 @@ type VertexMap struct {
 
 var m map[string]VertexMap
 
+var n = map[string]VertexMap{
+	"Bell Labs": {
+		1, 3,
+	},
+}
+
 func maps() {
+	fmt.Println(m == nil)
 	m = make(map[string]VertexMap)
 	m["Bell Labs"] = VertexMap{
 		1.1, -2,
 	}
 	fmt.Println(m["Bell Labs"])
+	fmt.Println(n["Bell Labs"])
 }
