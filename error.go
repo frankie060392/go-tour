a214d4e175c361d39bcbe7b26603f9ec9ab09e9f
Frankie <frankie@gmail.com>
2023-12-31 17:03:06 +0700

    Error excercise

--- error.go
+++ error.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -29,8 +30,31 @@ func runOk() string {
 	return "It works"
 }
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error(f float64) string {
+	return fmt.Sprintf("Can not sqrt Nagative sqrt number: %v", f)
+}
+
+func Sqrt(x float64) (float64, string) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x).Error(x)
+	}
+	return math.Sqrt(x), "Is Ok"
+}
+
 func errorFunc() {
 	if err := run(); err != nil {
 		fmt.Println(err)
 	}
+	test := &MyError{
+		time.Now(),
+		"It works",
+	}
+	if success := runOk(); success == "It works" {
+		fmt.Println(test)
+	}
+
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }
